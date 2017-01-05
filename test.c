4f37d8c29bd7c4e0663dec0a59f599036e3b4208
Yifan Yu <yifany@ucla.edu>
2017-01-04 17:41:06 -0800

    testing another case

--- test.c
+++ test.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 int main() {
-	int16_t block[9] = {5,10,12,6,10,8,7,3,9};	
+	int16_t block[16] = {0,0,0,0,0,5,1,12,0,6,10,8,0,7,3,9};	
 
-	printf("max slope is: %d\n", maxdegreeslope(&(block[0]), 3, 3, 3, 10.0, 10.0));
+	printf("max slope is: %d\n", maxdegreeslope(&(block[8]), 2, 2, 4, 10.0, 10.0));
 
 	return 0;
 }
