32b1c176a21def8c814cdb957d96747ff1509739
Yifan Yu <yifany@ucla.edu>
2017-01-04 17:24:10 -0800

    test function

---
+++ test.c
@@ -0,0 +1,11 @@
+//Testing maxdegreeslope function
+#include "maxdegreeslope.h"
+#include <stdio.h>
+
+int main() {
+	int16_t block[] = {5,10,12,6,10,8,7,3,9};	
+
+	printf("max slope is: %d\n", maxdegreeslope(&block, 3, 3, 3, 10.0, 10.0));
+
+	return 0;
+}
