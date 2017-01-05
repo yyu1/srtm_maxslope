78b0f9c9d1c4affc8c59dca5986ab8eea2640eee
Yifan Yu <yifany@ucla.edu>
2017-01-04 17:04:10 -0800

    add header file

---
+++ maxpctslope.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.1415929653589
+
+char maxdegreeslope(int16_t* ulpixel, int blockhorzsize, int blockvertsize, long imagehorzsize, float horzdistance, float vertdistance);
